8ef200974361c2fb521085958626a3bc0d561f6a
BillyvHoang <Billyhoang100@gmail.com>
2022-12-26 16:35:46 -0800

    Working page Translate

--- App.js
+++ App.js
@@ -5,18 +5,22 @@ import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
+
 export default function App() {
-  const [useNumber, setUserNumber] = useState();
+  const [userNumber, setUserNumber] = useState();
   
-  function pickedNumberHandler(pickedNumber) {
+  function pickedNumberHandler(pickedNumber)
+  {
     setUserNumber(pickedNumber);
   }
 
-  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />
+  let screen = <StartGameScreen  onPickNumber={pickedNumberHandler} />;
   
-  if (useNumber) {
-    screen = <GameScreen/>
+  if (userNumber) {
+    screen = <GameScreen />;
   }
+
+  
   return (
     <LinearGradient colors={["#3b021f", "#ddb52f"]} style={styles.rootScreen}>
       <ImageBackground source={require("./assets/images/background.png")} resizeMode="cover"
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,7 +3,8 @@ import { View, TextInput, StyleSheet, Alert} from "react-native";
 
 import PrimaryButton from "../Components/PrimaryButton";
 
-function StartGameScreen({ onPickedNumber}) {
+function StartGameScreen({onPickNumber}) {
+    
     const [enteredNumber, setEnteredNumber] = useState('');
     
     function numberInputHandler(enteredText) {
@@ -13,37 +14,40 @@ function StartGameScreen({ onPickedNumber}) {
     function resetInputHandler() {
         setEnteredNumber('');
     }
+
     function confirmInputHandler() {
         const chosenNumber = parseInt(enteredNumber);
 
         if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
             Alert.alert('Invalid number!', 'Number has to be a number between 1 and 99',
-                [{ text: 'Okay', style: "destructive", onPress:resetInputHandler }])
-                
+                [{ text: 'Okay', style: "destructive", onPress: resetInputHandler }]
+            );
             return;
-        }
-
-        onPickedNumber(chosenNumber);
+        } 
+        
+        onPickNumber(chosenNumber);
     }
-return (
-<View style={styles.inputContainer}>
-        <TextInput
-            style={styles.numberInput}
-            maxLength={2}
-            keyboardType="number-pad"
-            autoCapitalize="none"
-            value={enteredNumber}
-            onChangeText={numberInputHandler} />
 
-        <View style={styles.buttonsContainer}>
-            <View style={styles.buttonContainer}>
-                <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+    
+    return (
+    <View style={styles.inputContainer}>
+            <TextInput
+                style={styles.numberInput}
+                maxLength={2}
+                keyboardType="number-pad"
+                autoCapitalize="none"
+                value={enteredNumber}
+                onChangeText={numberInputHandler} />
+
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+                </View>
             </View>
-            <View style={styles.buttonContainer}>
-                <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
-            </View>
-        </View>
-</View>
+    </View>
 )}
 
 
